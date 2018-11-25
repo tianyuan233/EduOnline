eab5cbc6bbb37ab2c1560c4ca1ff782edb8b107a
tianyuan233 <1406282384@qq.com>
2018-11-25 19:44:58 +0800

    static

--- EduOnline/urls.py
+++ EduOnline/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import xadmin
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html"),name="index"),
 ]
