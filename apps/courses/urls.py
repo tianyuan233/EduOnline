ee0663cee62217359cb6ab6c6396c3f9f97232f4
tianyuan233 <1406282384@qq.com>
2019-01-11 23:31:22 +0800

    课程信息页

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -9,10 +9,13 @@
 """
 from django.urls import path, re_path
 
-from courses.views import CourseListView, CourseDetailView
+from courses.views import CourseListView, CourseDetailView, CourseInfoView
 
 app_name = "courses"
 urlpatterns = [
     path('list/', CourseListView.as_view(), name="list"),
+
     re_path('detail/(?P<course_id>\d+)/', CourseDetailView.as_view(), name="detail"),
+
+    re_path('info/(?P<course_id>\d+)/', CourseInfoView.as_view(), name="course_info"),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,11 +1,11 @@
-from pure_pagination import PageNotAnInteger, Paginator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic import View
+from pure_pagination import PageNotAnInteger, Paginator
 
-from courses.models import Course
-from operation.models import UserFavorite
+from courses.models import Course, CourseResource
+from operation.models import UserFavorite, UserCourse
 
 
 class CourseListView(View):
@@ -65,15 +65,43 @@ class CourseDetailView(View):
 
         return render(request, "course-detail.html",
                       {
-                          "course":course,
+                          "course": course,
                           "relate_courses": relate_courses,
                           "has_fav_course": has_fav_course,
                           "has_fav_org": has_fav_org,
                       })
 
 
+class CourseInfoView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    redirect_field_name = 'next'
 
+    def get(self, request, course_id):
+        # 此处的id为表默认为我们添加的值。
+        course = Course.objects.get(id=int(course_id))
 
-
-
-
+        # 查询用户是否开始学习了该课，如果还未学习则，加入用户课程表
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            course.students += 1
+            course.save()
+            user_course.save()
+        # 查询课程资源
+        all_resources = CourseResource.objects.filter(course=course)
+        # 选出学了这门课的学生关系
+        user_courses = UserCourse.objects.filter(course=course)
+        # 从关系中取出user_id
+        user_ids = [user_course.user_id for user_course in user_courses]
+        # 这些用户学了的课程,外键会自动有id，取到字段
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_course.course_id for user_course in all_user_courses]
+        # 获取学过该课程用户学过的其他课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums").exclude(id=course.id)[:4]
+        # 是否收藏课程
+        return render(request, "course-video.html", {
+            "course": course,
+            "all_resources": all_resources,
+            "relate_courses": relate_courses,
+        })
